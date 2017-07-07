f3a62fff28bf6f91654fe238de5da897f8758c83
mrashidcit <mrashidcit@gmail.com>
2017-07-07 17:24:05 +0500

    fresh material project app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCheckboxModule   } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdButtonModule,
     MdCheckboxModule,
-    
-
-    
-
-
   ],
   providers: [],
   bootstrap: [AppComponent]
