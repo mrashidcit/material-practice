bcd0070e1c89778857c8e2eb339064b23f5cc30f
mrashidcit <mrashidcit@gmail.com>
2017-07-09 07:11:18 +0500

    completed and working correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule, MdNativeDateModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
